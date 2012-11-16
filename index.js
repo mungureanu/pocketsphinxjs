069daa8788c4ad2087fb098825008b8db4b5a2f2
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-16 12:53:07 +0800

    reorder asynchronous steps

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var ffi = require('ffi');
-var path = require('path');
-var sys = require('sys');
-var events = require('events');
-var model = require('pocketsphinxjs-model');
-var testdata = require('pocketsphinxjs-testdata');
-var libs = require('pocketsphinxjs-libs');
 var assert = require('assert');
+var events = require('events');
+var ffi = require('ffi');
+var libs = require('pocketsphinxjs-libs');
+var model = require('pocketsphinxjs-model');
+var path = require('path');
 var ref = require('ref');
+var sys = require('sys');
+var testdata = require('pocketsphinxjs-testdata');
 
 var continuous = ffi.Library(libs.LIBPATH, {
    'init': ['pointer', [
@@ -28,89 +28,114 @@ function Recognizer(model) {
   var self = this;
   self.model = model;
   self.hypothesisEmitter = null;
-  self.started = false;
+  self.inited = false;
   self.device = 'plughw:0,0';
 
-  self.start = function(filename) {
-    assert(!self.started, 'Already started');
-    self.started = true;
+  self.start = function(cb) {
+    assert(!self.inited, 'Already started');
     continuous.init.async(
       self.model.hmm,
       self.model.lm,
       self.model.dict,
       function(err, init) {
-        self.init = init;
+        console.log('init completed');
         if(err) {
           console.error(err);
           self.emit('error', err);
-        }
-        continuous.wait_ready.async(self.init, function(err, isReady) {
-          if(isReady)
-            self.emit('ready');
-          else
-            self.emit('error', new Error('wait_ready: was waiting but never became READY'));
-        });
-
-        if(filename) {
-          continuous.recognize_from_file.async(self.init, filename, function(err) {
-            if(err)
-              emit('error', err);
-          });
+          cb(err);
         } else {
-          continuous.recognize_from_microphone.async(self.init, self.device, function(err) {
-            if(err)
-              emit('error', err);
-          });
+          self.init = init;
+          self.inited = true;
+          self.emit('inited');
         }
       }
     );
-    self.on('ready', function() {
-        console.log('starting polling of hypothesis');
-        self.hypothesisEmitter = function(err, hypothesis) {
-          if(err || !hypothesis) {
-            if(err) {
-              console.error(err);
-              emit('error', err);
-            }
-            console.error('stopping polling for hypothesis');
-            self.hypothesisEmitter = null;
-            return;
-          }
-          console.log('emitting ' + hypothesis)
-          self.emit('hypothesis', hypothesis);
-          continuous.get_next_hypothesis.async(self.init, self.hypothesisEmitter);
-        }
-        continuous.get_next_hypothesis.async(self.init, self.hypothesisEmitter);
+    self.once('inited', function() {
+      console.log('pocketsphinxjs return callback');
+      cb();
     });
   }
 
-  self.stop = function() {
-    assert(self.started, 'Cannot stop if not started');
-    continuous.stop(self.init);
-    self.started = false;
+  // self.poll = function() {
+  //   if(self.hypothesisEmitter) {
+  //     console.log('Already polling');
+  //     return;
+  //   }
+
+  //   console.log('starting polling of hypothesis');
+  //   self.hypothesisEmitter = function(err, hypothesis) {
+  //     if(err || !hypothesis) {
+  //       if(err) {
+  //         console.error(err);
+  //         emit('error', err);
+  //       }
+  //       console.error('stopping polling for hypothesis');
+  //       self.hypothesisEmitter = null;
+  //       return;
+  //     }
+  //     if(!self.inited) {
+  //       console.error('not inited, stop polling');
+  //       self.hypothesisEmitter = null;
+  //       return;
+  //     }
+  //     console.log('emitting ' + hypothesis)
+  //     self.emit('hypothesis', hypothesis);
+  //     continuous.get_next_hypothesis.async(self.init, self.hypothesisEmitter);
+  //   }
+  //   continuous.get_next_hypothesis.async(self.init, self.hypothesisEmitter);
+  // }
+
+  self.recognizeFromFile = function(filename) {
+    assert(filename);
+    assert(self.inited, 'cannot recognize if process not started');
+    continuous.recognize_from_file.async(self.init, filename, function(err) {
+      if(err) {
+        console.error(err);
+        emit('error', err);
+      }
+    });
+  }
+
+  self.recognizeFromMicrophone = function(cb) {
+    assert(self.inited, 'cannot recognize if process not started');
+    continuous.recognize_from_microphone.async(self.init, self.device, function(err) {
+      if(err) {
+        console.error(err);
+        emit('error', err);
+      }
+    });
+    continuous.wait_ready.async(self.init, function() {
+      console.log('recognizeFromMicrophone: wait ready returned');
+      cb();
+    });
+  }
+
+  self.stop = function(cb) {
+    assert(self.inited, 'Cannot stop if not started');
+    self.inited = false;
+    continuous.stop.async(self.init, cb);
   }
 
   self.getNextHypothesis = function(cb)  {
-    assert(self.started, 'Not started');
-    console.log('getting next hypothesis')
-    self.once('hypothesis', function(hypothesis) {
-      console.log('got hypothesis ' + hypothesis)
-      cb(null, hypothesis);
-    });
-  }
-
-  self.startOnReady = function(cb)  {
-    assert(!self.started, 'Already started');
-
-    self.once('error', function(err) {
-      console.error('error ' + err)
-      cb(err);
-    })
-    self.once('ready', function() {
-      console.log('callback for ready')
-      cb();
-    });
-    self.start();
+    //assert(self.inited, 'Not started');
+    // console.log('getting next hypothesis')
+    // self.once('hypothesis', function(hypothesis) {
+    //   console.log('got hypothesis ' + hypothesis)
+    //   cb(null, hypothesis);
+    // });
+    if(!self.inited) {
+      self.once('inited', function() {
+        continuous.wait_ready.async(self.init, function() {
+          console.log('getNextHypothesis 1: wait ready returned');
+          continuous.get_next_hypothesis.async(self.init, cb);
+        });
+      })
+    } else {
+      continuous.wait_ready.async(self.init, function() {
+        console.log('getNextHypothesis 2: wait ready returned');
+        continuous.get_next_hypothesis.async(self.init, cb);
+      });
+    }
   }
 
   events.EventEmitter.call(this);
--- test/recognizer.js
+++ test/recognizer.js
@@ -3,33 +3,59 @@
 var assert = require('should');
 var pocketsphinxjs = require('pocketsphinxjs');
 var testdata = require('pocketsphinxjs-testdata');
+var os = require('os');
+
 describe('audio', function() {
-  function testTurtleModel(done) {
-    pocketsphinxjs.startOnReady(function(err) {
+  var filename = testdata.GOFORWARD_RAW;
+
+  function checkHypothesis(done) {
+    pocketsphinxjs.getNextHypothesis(function(err, hypothesis) {
       if(err) return done(err);
-      console.log('ready');
-      pocketsphinxjs.getNextHypothesis(function(err, hypothesis) {
-        if(err) return done(err);
-        console.log('hypothesis received');
-        pocketsphinxjs.stop();
-        hypothesis.should.equal('go forward ten meters');
-        done();
-      });
-      testdata.play(testdata.GOFORWARD_RAW, function(err, message) {
-        if(err) return done(err);
-        console.log('played test audio');
-      });
+      console.log('TEST: hypothesis received');
+      hypothesis.should.equal('go forward ten meters');
+      pocketsphinxjs.stop(done);
     });
   }
 
+  function playAudio() {
+    testdata.play(filename, function(err, message) {
+      if(err) return done(err);
+      console.log('TEST: played test audio');
+    });
+  }
+
+  function startRecognition() {
+    pocketsphinxjs.start(function(err) {
+      if(err) return done(err);
+      console.log('TEST: before completed - ready');
+      if(os.hostname().indexOf('lophilo') != -1) {
+        pocketsphinxjs.recognizeFromMicrophone(function(err) {
+          if(err) throw new Error(err);
+          playAudio();
+        });
+      } else {
+        pocketsphinxjs.recognizeFromFile(filename);
+      }
+    });
+  }
+
+  beforeEach(function() {
+    console.log('TEST: >>>>>>>>>>>>>>>>>>>>>>>');
+  });
+
   it('played audio is recognized', function(done) {
-    this.timeout(60000);
-    testTurtleModel(done);
+    checkHypothesis(done);
+    startRecognition();
   });
 
   it('recognition can successfully be restarted', function(done) {
-    this.timeout(60000);
-    testTurtleModel(done);
+    checkHypothesis(done);
+    startRecognition();
   });
+
+  afterEach(function() {
+    console.log('TEST: <<<<<<<<<<<<<<<<<<<<<<<<');
+  });
+
 });
 
